5707d1e1600f00a027ac96ab278895143c633198
ajspryn <adjisupriyono@gmail.com>
2026-05-25 08:24:34 +0700

    style: Improve code formatting for logo and favicon URL methods in AppSetting model

--- app/Models/AppSetting.php
+++ app/Models/AppSetting.php
@@ -101,11 +101,11 @@ class AppSetting extends Model
     public static function getLogoUrl($default = null)
     {
         $logoPath = static::getSetting('app_logo');
-        if ($logoPath && file_exists(public_path('storage/'.$logoPath))) {
-            $fullPath = public_path('storage/'.$logoPath);
+        if ($logoPath && file_exists(public_path('storage/' . $logoPath))) {
+            $fullPath = public_path('storage/' . $logoPath);
             $mtime = filemtime($fullPath);
 
-            return asset('storage/'.$logoPath).'?v='.$mtime;
+            return asset('storage/' . $logoPath) . '?v=' . $mtime;
         }
 
         return $default ?: asset('template/images/logos/logo.png');
@@ -117,11 +117,11 @@ class AppSetting extends Model
     public static function getFaviconUrl($default = null)
     {
         $faviconPath = static::getSetting('app_favicon');
-        if ($faviconPath && file_exists(public_path('storage/'.$faviconPath))) {
-            $fullPath = public_path('storage/'.$faviconPath);
+        if ($faviconPath && file_exists(public_path('storage/' . $faviconPath))) {
+            $fullPath = public_path('storage/' . $faviconPath);
             $mtime = filemtime($fullPath);
 
-            return asset('storage/'.$faviconPath).'?v='.$mtime;
+            return asset('storage/' . $faviconPath) . '?v=' . $mtime;
         }
 
         return $default ?: asset('template/images/favicons/favicon.ico');
